f3c7978ec7e1fb03a805e2369a5e99bfb11cdd90
koipish <koipish@gmail.com>
2025-01-04 18:11:45 +0300

    added tests

--- workers/tests.py
+++ workers/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+from workers.models import Worker
+
+
+class WorkerTestCase(TestCase):
+    def setUp(self):
+        # Create a worker for testing
+        self.worker = Worker.objects.create(
+            full_name="Коровин",
+            brigade_number=1,
+            salary=15000,
+            specialization="Чистовая отделка",
+        )
+
+    def test_worker_retrieve(self):
+        url = reverse("workers:worker_retrieve", args=(self.worker.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data["full_name"], self.worker.full_name)
+
+    def test_BrigadeWorkerList(self):
+        """
+        Проверяем получение списка работников в бригаде
+        """
+        url = reverse("workers:brigade_workers_list", args=(self.worker.brigade_number,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
